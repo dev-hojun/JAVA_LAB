57551aaa581b80fd0793fc5e30a7551fba644e40
이호준 <juns9050@gmail.com>
2021-09-03 18:21:38 +0900

    21.09.03 제어 흐름 이해하기(2)

--- Chapter4/src/loopexample/WhileExample.java
+++ Chapter4/src/loopexample/WhileExample.java
@@ -3,8 +3,14 @@ package loopexample;
 public class WhileExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int num = 1;
+		int sum = 0; // 초기화를 해 줘야 오류가 안 남
+		
+		do { // 블럭 안에 조건이 무조건 한 번은 수행됨
+			sum += num;
+			num++;
+		} while(num <= 10);
+		
+		System.out.println("1부터 10까지의 합은 " + sum + "입니다.");
 	}
-
 }
