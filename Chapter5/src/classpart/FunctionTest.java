6074898c0b414aa2a1b0920e13a7a2414588a3b1
이호준 <juns9050@gmail.com>
2021-09-08 14:33:30 +0900

    21.09.08 클래스와 객체(3)

--- Chapter5/src/classpart/FunctionTest.java
+++ Chapter5/src/classpart/FunctionTest.java
@@ -6,12 +6,12 @@ public class FunctionTest {
 		int num1 = 10;
 		int num2 = 30;
 		
-		int sum = addNum(num1, num2);
-		System.out.println(sum);
+		int sum = addNum(num1, num2); // return에서 반환된 값이 sum으로 들어감
+		System.out.println(sum); // 출력
 	}
 
-	public static int addNum(int n1, int n2) {
-		int result = n1 + n2;
-		return result;
+	public static int addNum(int n1, int n2) { // n1, n2는 addNum에서 새로 선언된 변수, 값을 복사해서 받는 역할만 함
+		int result = n1 + n2; // 복사한 값을 더한 후
+		return result; // 리턴으로 반환
 	}
 }
--- Chapter5/src/classpart/Student.java
+++ Chapter5/src/classpart/Student.java
@@ -7,9 +7,42 @@ public class Student {
 	int grade; // 정수
 	String address; // 문자열
 	
+	public Student() {}
+	
+	public Student(int id, String name) { // Student가 생성될 때 두 개의 매개변수를 전달받아 생성
+		studentID = id;
+		studentName = name;
+	}
+	
 	public void showStudentInfo() {
 		System.out.println(studentName + "," + address); // <- 메서드
 	}
 	
+	public String getStudentName() {
+		return studentName;
+	}
 	
+	public void setStudentName(String name) {
+		studentName = name;
+	}
+	 
+	public static void main(String[] args) {
+		
+		Student studentLee = new Student();
+		studentLee.studentName = "이순신";
+		studentLee.studentID = 100;
+		studentLee.address = "서울시 영등포구 여의도동";
+		
+		Student studentKim = new Student();
+		studentKim.studentName = "김유신";
+		studentKim.studentID = 101;
+		studentKim.address = "미국 산호세";
+		
+		studentLee.showStudentInfo();
+		studentKim.showStudentInfo();
+		
+		System.out.println(studentLee);
+		System.out.println(studentKim);
+		
+	}
 }
