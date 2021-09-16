3ee3d2ef089787beb67d71e97ee2c2d47c027d07
이호준 <juns9050@gmail.com>
2021-09-16 15:01:53 +0900

    21.09.16 print, println

--- Chapter7/src/print/PrintTest.java
+++ Chapter7/src/print/PrintTest.java
@@ -3,6 +3,12 @@ package print;
 public class PrintTest {
 
 	public static void main(String[] args) {
+		System.out.println("========print=========");
+		System.out.print("Hello everyone!\n");
+		System.out.print("Hello everyone!\n");
+		
+		
+		System.out.println("======println========");
 		System.out.println("Hello Would!");
 		System.out.println("Hello everyone!");
 	}
